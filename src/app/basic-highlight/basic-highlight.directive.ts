0fab3f3b9ffc25df7b7f6662495124d9a2a852d8
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-20 20:21:02 +0530

    Commit#27

--- src/app/basic-highlight/basic-highlight.directive.ts
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -1,11 +1,15 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
 selector: '[appBasicHighlight]'
 })
 
-export class BasicHighlightDirective{
+export class BasicHighlightDirective implements OnInit{
 	constructor(private elementRef: ElementRef) {
+
+	}
+
+	ngOnInit(){
 
 	}
 }
