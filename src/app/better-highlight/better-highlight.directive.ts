fc5e7e0f89f207ee56e444b8302a963b6420153d
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-22 18:07:42 +0530

    Commit#51

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -11,6 +11,7 @@ export class BetterHighlightDirective implements OnInit {
 
   ngOnInit() {
   	this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  	this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
   }
 
 }
